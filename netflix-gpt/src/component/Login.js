3df659347ee387b6e312bdf585c1dd42e25cb23c
sartkthegr8 <49450634+sartkthegr8@users.noreply.github.com>
2023-11-13 07:49:48 +0000

    #

--- netflix-gpt/src/component/Login.js
+++ netflix-gpt/src/component/Login.js
@@ -3,12 +3,17 @@ import Header from './Header';
 
 const Login = () => {
   return (
-    <div>
+    <div >
       <Header/>
-      <div>
+      <div className='absolute'>
       <img src='https://assets.nflxext.com/ffe/siteui/vlv3/77d35039-751f-4c3e-9c8d-1240c1ca6188/cf244808-d722-428f-80a9-052acdf158ec/IN-en-20231106-popsignuptwoweeks-perspective_alpha_website_small.jpg'
       alt='backgroud-image'/>;
       </div>
+      <form className='w-3/12 relative p-12 bg-black'>
+        <input type='text' placeholder='Email Address' className='p-2 m-2'/>
+        <input type='password' placeholder='Password' className='p-2 m-2'/>
+        <button className='p-4 m-4'>Sign In</button>      
+      </form>
     </div>
   )
 }
